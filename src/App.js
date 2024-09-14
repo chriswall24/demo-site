e4726db79b85203b7c93d245176e70eca7bb5aa1
Chris Wall <cbwall24@gmail.com>
2024-09-14 12:32:35 -0700

    finished hero section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Home from "./components/pages/Home";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -8,7 +9,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact />
+          <Route path="/" exact Component={Home} />
         </Routes>
       </Router>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
-import './Navbar.css';
+import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -11,26 +11,24 @@ function Navbar() {
   const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
-    if(window.innerWidth <= 960) {
-      setButton(false)
+    if (window.innerWidth <= 960) {
+      setButton(false);
     } else {
-      setButton(true)
+      setButton(true);
     }
   };
 
   useEffect(() => {
-    showButton(
+    showButton();
+  }, []);
 
-    )
-  }, [])
-
-  window.addEventListener('resize', showButton);
+  window.addEventListener("resize", showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             LOGO <i className="fab fa-typo3" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
@@ -66,7 +64,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
-          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </>
