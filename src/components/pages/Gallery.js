9275ebf0e2b7a5ffcac49170d0d19c5cd6ddbc20
Chris Wall <cbwall24@gmail.com>
2024-09-18 20:46:59 -0700

    adding footer to gallery

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "../../App.css";
 import GallerySection from "../GallerySection";
+import Footer from '../Footer';
+
 
 export default function Gallery() {
   return (
     <>
       {/* <h1 className="gallery"> GALLERY</h1> */}
       <GallerySection />
+      <Footer />
     </>
   );
 }
